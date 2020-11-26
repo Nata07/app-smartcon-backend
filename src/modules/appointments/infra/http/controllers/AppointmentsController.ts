52bb0bc32a49e9d14d67a4e6b1a14bc66a1a07f3
Natanael <nata.mw@hotmail.com>
2020-11-25 22:38:57 -0300

    fix: add validations user exists

--- src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -2,30 +2,38 @@ import CreateUserService from '@modules/users/services/CreateUserService';
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
 export default class AppointmentsController {
   public async create(request: Request, response: Response): Promise<Response> {
     const createUser = container.resolve(CreateUserService);
     const createAppointment = container.resolve(CreateAppointmentService);
+    const userRepository = new UsersRepository();
 
+    let userId = '';
     const { provider_id, date, name, email, phone, permission } = request.body;
 
     const password = 'smartcon2020';
+    const userExist = await userRepository.findByEmail(email);
 
-    const user = await createUser.execute({
-      name,
-      email,
-      phone,
-      password,
-      permission,
-    });
+    if (userExist) {
+      userId = userExist.id;
+    } else {
+      const user = await createUser.execute({
+        name,
+        email,
+        phone,
+        password,
+        permission,
+      });
 
-    const user_id = user.id;
+      userId = user.id;
+    }
 
     const appointment = await createAppointment.excecute({
-      provider_id,
       date,
-      user_id,
+      provider_id,
+      user_id: userId,
     });
 
     return response.json(appointment);
